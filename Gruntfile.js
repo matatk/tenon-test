6b154bbeb527964f9e40bcdfe8687fd2cdada7b4
Matthew Tylee Atkinson <matatk@gmail.com>
2016-07-05 20:28:59 +0100

    Switch to custom code for SVG conversion
    This comes from the landmarks extension on which I also work.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		respimg: {
+		svg2png: {
 			chrome: {
 				options: {
 					widths: [
@@ -16,13 +16,10 @@ module.exports = function(grunt) {
 						38,  // Chrome  (tooblar x2)
 						48,  // Both    (general)
 						128  // Chrome  (store)
-					],
-					optimize: false
+					]
 				},
 				files: [{
-					expand: true,
-					cwd: 'src/build/',
-					src: ['*.svg'],
+					src: 'src/build/*.svg',
 					dest: 'extension/chrome/'
 				}]
 			},
@@ -35,31 +32,76 @@ module.exports = function(grunt) {
 						48,  // Both    (general)
 						64,  // Firefox (menu panel x2)
 						96   // Firefox (general x2)
-					],
-					optimize: false
+					]
 				},
 				files: [{
-					expand: true,
-					cwd: 'src/build/',
-					src: ['*.svg'],
+					src: 'src/build/*.svg',
 					dest: 'extension/firefox/'
 				}]
 			}
 		},
 
-		clean: {
-			built: [
-				'extension/',
-			],
-			todo: [
-				'extension/**/*.svg'  // TODO remove after image-gen sorted out
-			]
+		jshint: {
+			options: {
+				jshintrc: true
+			}
 		}
+	});
+
+	// As I can't seem to find a grunt package to actually do this...
+	grunt.registerMultiTask('svg2png', 'Convert SVGs to PNGs via ImageMagick', function() {
+		var path = require('path');
+		var widths = this.data.options.widths;
+		var conversionsDone = 0;
+
+		this.files.forEach(function(f) {
+			f.src.filter(function(filepath) {
+				widths.forEach(function(width) {
+					// Assume filename ends in '.svg'
+					var baseFileName = path.basename(filepath).slice(0, -4);
+					var options = {
+						cmd: 'convert',
+						stdio: 'inherit',
+						args: [
+							'-background',
+							'transparent',
+							'-resize',
+							width + 'x' + width,
+							filepath,
+							f.dest + baseFileName + '-' + width + '.png'
+						]
+					};
+
+					grunt.verbose.writeln('Conversion task:', options);
+
+					grunt.util.spawn(options, function(error, result, code) {
+						if (error) {
+							grunt.log.error('convert exit code:', code);
+							throw(error);
+						} else {
+							conversionsDone += 1;
+							grunt.verbose.writeln(conversionsDone,
+								'SVG to PNG conversions complete.');
+						}
+					});
+				});
+			});
+		});
 	});
 
 	// The following task declarations are even more repetitive,
 	// so declare them in a loop
 	['firefox', 'chrome'].forEach(function(browser) {
+		grunt.config.set('clean.' + browser, [
+			'extension/' + browser
+		]);
+
+		grunt.config.set('mkdir.' + browser, {
+			options: {
+				create: ['extension/' + browser]
+			}
+		});
+
 		grunt.config.set('json_merge.' + browser, {
 			files: [{
 				dest: 'extension/' + browser + '/manifest.json',
@@ -79,16 +121,21 @@ module.exports = function(grunt) {
 			}]
 		});
 
+		grunt.config.set('jshint.' + browser, [
+			'extension/' + browser + '/*.js'
+		]);
+
 		grunt.registerTask(browser, [
-			'respimg:' + browser,
+			'clean:' + browser,
+			'mkdir:' + browser,
+			'svg2png:' + browser,
 			'copy:' + browser,
 			'json_merge:' + browser,
-			'clean:todo'
+			'jshint:' + browser,
 		]);
 	});
 
 	grunt.registerTask('default', [
-		'clean:built',
 		'chrome',
 		'firefox'
 	]);
